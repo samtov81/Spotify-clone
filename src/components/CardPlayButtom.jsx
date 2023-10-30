e7c19ce39287f75565af5be4e0a7a500a3b642a3
Samuel Tovar <sato.wd@gmail.com>
2023-10-30 19:16:56 -0400

    Mejoras de visualizacion y transiciones

---
+++ src/components/CardPlayButtom.jsx
@@ -0,0 +1,19 @@
+import { Pause, Play } from "./Player"
+import { usePlayerStore } from "@/store/playerStore";
+
+
+export function CardPlayButtom ({id}) {
+  const {isPlaying, currentMusic, setIsPlaying, setCurrentMusic} = usePlayerStore(state => state)
+  
+  const handleClick = () => {
+    // setIsPlaying(!isPlaying)
+    setCurrentMusic({playlist:{id}})
+    setIsPlaying(!isPlaying)
+  }
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
+  return(
+    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
+      {isPlayingPlaylist ? <Pause/> : <Play/>}
+    </button>
+  )
+}
