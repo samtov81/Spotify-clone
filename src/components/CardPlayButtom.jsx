fa7051b0d4b8d86c69b49ab3be6e2f2ef8952f40
Samuel Tovar <sato.wd@gmail.com>
2023-11-22 19:38:15 -0400

    Modificacion del player

--- src/components/CardPlayButtom.jsx
+++ src/components/CardPlayButtom.jsx
@@ -2,7 +2,7 @@ import { Pause, Play } from "./Player"
 import { usePlayerStore } from "@/store/playerStore";
 
 
-export function CardPlayButtom ({id}) {
+export function CardPlayButtom ({id, size = 'small'}) {
   const {isPlaying, currentMusic, setIsPlaying, setCurrentMusic} = usePlayerStore(state => state)
   
   const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
@@ -19,12 +19,14 @@ export function CardPlayButtom ({id}) {
       const {songs, playlist } = data
       setIsPlaying(true)
       setCurrentMusic({songs, playlist, song: songs[0]})
-      console.log({songs, playlist})
     })
   }
+
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
+
   return(
-    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-      {isPlayingPlaylist ? <Pause/> : <Play/>}
+    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+    {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
     </button>
   )
 }
